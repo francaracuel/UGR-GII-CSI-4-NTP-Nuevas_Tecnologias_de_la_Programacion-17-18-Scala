2d4e389a99661eb2ea84eddb219131aa5412d861
Francisco Javier Caracuel Beltrán <fjcb312@gmail.com>
2018-03-07 10:32:20 +0100

    Base

---
+++ practica1/src/listado/Ruta.java
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Francisco Javier Caracuel Beltrán
+//
+// Nuevas Tecnologías de la Programación (NTP) - CSI - UGR - ETSIIT
+//
+// Curso 2017/2018
+//
+// Ruta.java
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package listado;
+
+/**
+ * Enumerado para representar los códigos de las rutas
+ * de la empresa
+ */
+public enum Ruta {
+    RUTA1, RUTA2, RUTA3, NORUTA
+}
---
+++ practica1/src/listado/Sector.java
@@ -0,0 +1,21 @@
+///////////////////////////////////////////////////////////////////////////////
+//
+// Francisco Javier Caracuel Beltrán
+//
+// Nuevas Tecnologías de la Programación (NTP) - CSI - UGR - ETSIIT
+//
+// Curso 2017/2018
+//
+// Sector.java
+//
+///////////////////////////////////////////////////////////////////////////////
+
+package listado;
+
+/**
+ * Enumerado para representar los sectores de actuacion de
+ * los comerciales
+ */
+public enum Sector {
+    SECTOR1, SECTOR2, NOSECTOR
+}
